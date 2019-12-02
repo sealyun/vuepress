710b9e0c058559801ffd062acb5389b3971b4f49
fanux <fhtjob@hotmail.com>
2019-12-02 23:12:41 +0800

    update

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,7 @@
 vuepress build
+git add .
+git commit -m "update"
+git push
 cp -r .vuepress/dist/* ~/work/src/github.com/sealyun/sealyun.github.io
 cd ~/work/src/github.com/sealyun/sealyun.github.io
 git add .
